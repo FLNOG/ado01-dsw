7a32588f1b6f5b4556b30983aefe9582bd734f01
Felipe Nogueira <felipe@tecnbr.com.br>
2025-08-14 21:59:37 -0300

    Alteração da index.html

---
+++ src/main/java/br/com/carstore/servlet/CreateCarServlet.java
@@ -0,0 +1,26 @@
+package br.com.carstore.servlet;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet("/create-car")
+public class CreateCarServlet extends HttpServlet {
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String carName = request.getParameter("car-name");
+        String carColor = request.getParameter("car-color");
+        System.out.println(carName + " " + carColor);
+
+        request.getRequestDispatcher("index.html").forward(request, response);
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        
+    }
+}
